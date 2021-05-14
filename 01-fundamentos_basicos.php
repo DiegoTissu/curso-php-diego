8f83507ec7da5025997fff5c4cb52a46d195b455
diegotissu <diegotissu@hotmail.com>
2021-05-14 20:17:07 -0300

    Subindo arquivos do Bootstrap

---
+++ 01-fundamentos_basicos.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Fundamentos básicos</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    
+    <?php
+
+    //Esse é meu primeiro código PHP
+
+    /* Testando meu primento código PHP */
+
+    echo "Meu nome é Diego <br>";
+    echo "Minha idade é 31";
+
+    ?>
+
+  </body>
+</html>
+
