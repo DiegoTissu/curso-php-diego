0de5618cd1ca758270de8a3ed5e433d90b2b4cf1
diegotissu <diegotissu@hotmail.com>
2021-05-14 20:04:50 -0300

    Arquivo de variaveis

---
+++ 02-variaveis.php
@@ -0,0 +1,15 @@
+<?php
+
+//Meus dados
+
+    $nome = "Diego";
+    $idade = "31";
+    $altura = "1,86";
+
+    echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura";
+
+//Alterando meus dados
+
+
+
+?>
