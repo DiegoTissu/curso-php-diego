55db39b03d0d5ec4250e20ec6d10809e95675511
diegotissu <diegotissu@hotmail.com>
2021-05-14 20:09:35 -0300

    Fiz um alinhmamento

--- 02-variaveis.php
+++ 02-variaveis.php
@@ -1,15 +1,9 @@
 <?php
 
 //Meus dados
+$nome = "Diego";
+$idade = "31";
+$altura = "1,86";
 
-    $nome = "Diego";
-    $idade = "31";
-    $altura = "1,86";
-
-    echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura";
-
-//Alterando meus dados
-
-
-
-?>
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura";
+//Alterando meus dado
