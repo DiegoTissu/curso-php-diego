1fb07079163d3aaba7054b42c66fe357659d2ca5
diegotissu <diegotissu@hotmail.com>
2021-05-18 14:28:31 -0300

    Alterando dados e adicionando dados

--- 02-variaveis.php
+++ 02-variaveis.php
@@ -1,8 +1,23 @@
 <?php
 //Meus dados
-$nome = "Diego Miranda";
-$idade = "31";
-$altura = "1,86";
+    $nome = "Diego Miranda";
+    $idade = "31";
+    $altura = "1,86";
 
-echo "Nome correto: $nome, minha idade é $idade e minha altura é $altura";
-//Alterando meus dado
+    echo "Nome: $nome, <br> Idade: $idade <br> Altura: $altura";
+
+    
+    echo "<hr>";
+    echo "<br>";
+
+//Adicionando dados
+
+    $usuario = "Jean Paul";
+    $email = "contato@agenciacriamais.com.br";
+    $cidade = "Cidade Ocidental"; 
+
+    echo "Nome do usuário: $usuario, <br> E-mail: $email <br> Residência: $cidade";
+    
+
+   
+
