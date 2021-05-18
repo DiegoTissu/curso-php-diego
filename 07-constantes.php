8bcc02f94928ea5c4d61247ce5e221ec2d8df6ac
diegotissu <diegotissu@hotmail.com>
2021-05-18 17:38:03 -0300

    Aprendendo constantes

---
+++ 07-constantes.php
@@ -0,0 +1,14 @@
+<?php
+//Constantes
+
+    define("NOME", "Diego Miranda");
+    define("IDADE", 31);
+    define("ALTURA", 1.86);
+    define("SOLTEIRO", true);
+
+    define("TIMES", ['Gama', 'Flamengo', 'Barcelona']);
+        
+    echo 'Meu nome é '.NOME.', Minha idade '.IDADE.', Minha altura '.ALTURA.'.' ;
+    echo "<hr>";
+    var_dump(TIMES);
+
