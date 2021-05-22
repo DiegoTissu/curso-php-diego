564cce11f4dbf4fbbf5e8e3e88a187f73da7923b
diegotissu <diegotissu@hotmail.com>
2021-05-22 14:22:05 -0300

    Aprendendo arrays

---
+++ 08-arrays.php
@@ -0,0 +1,11 @@
+<?php
+//Arrays
+
+    $carros = array(1=>"Duster", 2=> "HB20", 3=> "Gol");
+    $carros[4] = "compass";
+    echo $carros[1];
+
+    echo "<br>";
+
+    $clientes = array("Jean", "Tiago", "Calebe");
+    echo $clientes[0];
