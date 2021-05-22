6cb382e1fcaa0c463f4c7c4f88694d35eb047c28
diegotissu <diegotissu@hotmail.com>
2021-05-22 14:45:41 -0300

    Aula 13, cont e foreach

--- 08-arrays.php
+++ 08-arrays.php
@@ -4,8 +4,21 @@
     $carros = array(1=>"Duster", 2=> "HB20", 3=> "Gol");
     $carros[4] = "compass";
     echo $carros[1];
-
     echo "<br>";
 
-    $clientes = array("Jean", "Tiago", "Calebe");
-    echo $clientes[0];
+    $clientes = ["Jean", "Tiago", "Calebe"];
+    $clientes[3] = "Valdeci";
+    print_r($clientes);
+    echo "<hr>";
+
+    //Cont
+
+    $totalClientes = count($clientes);
+    echo "$totalClientes";
+    echo "<hr>";
+
+    //Foreach
+
+    foreach($clientes as $valor){
+        echo $valor."<br>";    
+        }
