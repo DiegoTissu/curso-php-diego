14ee5d1a25f1e59c5b76b07e3f090e4aa557879d
diegotissu <diegotissu@hotmail.com>
2021-06-29 19:03:01 -0300

    Operadores de Atribuição

---
+++ 13-operadores-de-atribuicao.php
@@ -0,0 +1,24 @@
+<?php
+// Operadores de Atribuição 
+
+$a = 10;
+$b = 5;
+
+$a += $b; //$a = $a + $b 
+    echo $a;  
+echo "<hr>";
+
+$a -= $b; //$a = $a - $b 
+    echo $a;
+echo "<hr>";
+
+$a *= $b; //$a = $a * $b 
+    echo $a;
+echo "<hr>";
+
+$a /= $b; //$a = $a / $b 
+    echo $a;
+echo "<hr>";
+
+$a %= $b; //$a = $a % $b 
+    echo $a;
