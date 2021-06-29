308ccb7ff4ae96774ab6fdc5287cb8f7d061e556
diegotissu <diegotissu@hotmail.com>
2021-06-29 19:03:29 -0300

    Operadores de Comparação

---
+++ 14-operadores-de-comparacao.php
@@ -0,0 +1,23 @@
+<?php
+//Operadores de Comparação
+// ==
+// !=
+// ===
+// !===
+// <>
+// <
+// >
+// <=
+// >=
+// <=>
+
+if(10 == 10):
+        echo "Positivo.";
+else:
+        echo "Negativo.";
+endif;
+
+echo "<hr>";
+
+var_dump(10 <=> 20);
+
