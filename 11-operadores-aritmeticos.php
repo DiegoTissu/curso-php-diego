128b77037b3dd05823b08ce4e2932c6a17f77159
diegotissu <diegotissu@hotmail.com>
2021-06-29 19:01:26 -0300

    Operadores Arimeticos: São usados com valores numéricos para executar operações. * Aritméticas comuns são: * como adição + * subtração - * multiplicação * * divisão / * Módulo % * Exponenciação **

---
+++ 11-operadores-aritmeticos.php
@@ -0,0 +1,44 @@
+<?php
+/* OPERADORES ARITMETICOS 
+* São usados com valores numéricos para executar operações.
+* Aritméticas comuns são:
+* como adição + 
+* subtração -
+* multiplicação *
+* divisão /
+* Módulo %
+* Exponenciação **
+*/
+
+$a = 10;
+$b = 20;
+$c = 30;
+$d = 5;
+$e = 16;
+
+$adicao = $a + $b;
+    echo "Resultado adição:", $adicao;
+echo "<hr>";
+
+$subtracao = $c - $d;
+    echo "Resultado subtração:", $subtracao;
+echo "<hr>";
+
+$multiplicacao = $b * $c;
+    echo "Resultado multiplicação:", $multiplicacao;
+echo "<hr>";
+
+$divicao = $c / $a;
+    echo "Resultado divisão:", $divicao;
+echo "<hr>";
+
+$modulo = $d % $a;
+    echo "Resultado modulo:", $modulo;
+echo "<hr>";
+
+$expo = $d ** $a;
+    echo "Resultado expoente:", $expo;
+echo "<hr>";
+
+echo (5 + 7 + 10 + 20) / 50;
+
