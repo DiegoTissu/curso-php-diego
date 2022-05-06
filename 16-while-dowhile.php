f08627dd20b95da8c0a6eaa7642d5db5606940ef
diegotissu <diegotissu@hotmail.com>
2022-05-06 01:59:51 -0300

    Comandos de While & Do While

---
+++ 16-while-dowhile.php
@@ -0,0 +1,24 @@
+<?php
+// While & Do While
+
+// While significa ENQUANTO
+
+$contador = 1;
+
+while ($contador <= 10):
+    echo "Contador é $contador <br>";
+    $contador++;
+
+endwhile;
+
+echo "<hr>";
+
+//Do seginifica: FAÇA
+
+$contador = 1;
+
+do {
+    echo "Contador é $contador <br>";
+    $contador++;
+} while ($contador <= 10);
+
