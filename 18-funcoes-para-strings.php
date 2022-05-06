b55db48c20c984299391c51f15acdea5ba1ff47b
diegotissu <diegotissu@hotmail.com>
2022-05-06 02:02:56 -0300

    funçoes para strings: strtoupper, strtolower, substr, str_pad, str_repeat, strlen, str_replace, strpos.

---
+++ 18-funcoes-para-strings.php
@@ -0,0 +1,55 @@
+<?php
+/* funçoes para strings
+strtoupper
+strtolower
+substr
+str_pad
+str_repeat
+strlen
+str_replace
+strpos
+*/
+
+//strtoupper
+$nome = "diego miranda";
+$novoNome = strtoupper($nome);
+echo $novoNome;
+
+echo "<br>";
+
+//strtolower
+$nome = "DIEGO MIRANDA";
+$novoNome = strtolower($nome);
+echo $novoNome;
+
+echo "<hr>";
+
+$mensagem = "Olá, Mundo!";
+echo substr($mensagem, 4, 6);
+
+echo "<hr>";
+
+$objeto = "iPhone";
+$novoObjeto = str_pad($objeto, 10, "*", STR_PAD_LEFT);
+echo $novoObjeto;
+
+echo "<hr>";
+
+//str_repeat
+$string = str_repeat("Sucesso!", 5);
+echo $string; 
+
+echo "<hr>";
+
+//strlen
+$mensagem = "Olá, Mundo!";
+echo strlen($mensagem);
+
+echo "<hr>";
+
+//str_replace
+$texo = "A Seleção Argentina será a campeã da copa do mundo de 2022.";
+$novoTexto = str_replace("Argentina", "Brasileira", $texto);
+echo $novoTexto;
+
+
