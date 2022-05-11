554d84a966fed2ea8b4d2b7f5952b03ed8a84e5f
diegotissu <diegotissu@hotmail.com>
2022-05-11 04:40:12 -0300

    Fazendo formulário e enviando informações ($_GET)

---
+++ 22-formularios/dados.php
@@ -0,0 +1,10 @@
+<?php
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+
+echo "Seu nome é $nome e seu Email é $email";
+
+echo "<br>";
+
+var_dump($_POST); 
---
+++ 22-formularios/dados2.php
@@ -0,0 +1,7 @@
+<?php
+
+echo $_GET['idade']."<br>".$_GET['sobrenome'];
+
+echo "<br>";
+
+var_dump($_POST); 
